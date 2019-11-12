27bb13a9ed77d31760ab923f6ca9a1c114865737
dan demeo <chronos937@gmail.com>
2019-11-12 01:01:02 -0500

    fixed default omdb movie search link, bands in town still not working: getting error that result is undefined but should be within scope; still don't quite get the spotify api

--- liri.js
+++ liri.js
@@ -15,10 +15,10 @@ if (request === "artist-event") {
         // if (err) {
         //     console.log(err);
         // }
-        console.log(result);
-        for (i = 0; i < result.length; i++) {
+        // console.log(result);
+        for (i = 0; i < 5; i++) {
             console.log("============================");
-            console.log(moment(result[i].datetime));
+            console.log(result[i].datetime);
             console.log("============================");
             console.log("Venue: " + result[i].venue.name);
             console.log("============================");
@@ -30,40 +30,56 @@ if (request === "artist-event") {
 }
 
 if (request === "movie") {
-    var title = process.argv.slice(3).join("+");
-    axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(function (response) {
-        var info = response.data;
-        // console.log(response);
-        console.log("Title: " + info.Title);
-        console.log("Release Year: " + info.Year);
-        console.log("imdbRating: " + info.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + info.Ratings[1].Value);
-        console.log("Country: " + info.Country);
-        console.log("Language: " + info.Language);
-        console.log("Plot: " + info.Plot);
-        console.log("Actors: " + info.Actors);
-    })
-        .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
+    if (process.argv.length === 3) {
+        axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(function (response) {
+            var info = response.data;
+            console.log("Title: " + info.Title);
+            console.log("Release Year: " + info.Year);
+            console.log("imdbRating: " + info.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + info.Ratings[1].Value);
+            console.log("Country: " + info.Country);
+            console.log("Language: " + info.Language);
+            console.log("Plot: " + info.Plot);
+            console.log("Actors: " + info.Actors);
 
+        })
+    }
+    else {
+        var title = process.argv.slice(3).join("+");
+        axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(function (response) {
+            var info = response.data;
+            // console.log(response);
+            console.log("Title: " + info.Title);
+            console.log("Release Year: " + info.Year);
+            console.log("imdbRating: " + info.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + info.Ratings[1].Value);
+            console.log("Country: " + info.Country);
+            console.log("Language: " + info.Language);
+            console.log("Plot: " + info.Plot);
+            console.log("Actors: " + info.Actors);
+        })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+
+            });
+    }
 }
 
 if (request === "music") {
